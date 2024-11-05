15d5dfd5a468482f2c32c2a0862fd11fccf140b1
iMelamory <iMelamory>
2024-11-05 09:51:37 -0500

    module 5 done

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -15,9 +15,7 @@ def initiate():
         car_make_instances.append(
             CarMake.objects.create(
                 name=data["name"],
-                description=data["description"],
-                country=data["country"],
-                year=data["year"],
+                description=data["description"]
             )
         )
 
@@ -28,105 +26,90 @@ def initiate():
             "type": "SUV",
             "year": 2023,
             "car_make": car_make_instances[0],
-            "price": 32000,
         },
         {
             "name": "Qashqai",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[0],
-            "price": 47000,
+            "car_make": car_make_instances[0]
         },
         {
             "name": "XTRAIL",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[0],
-            "price": 22000,
+            "car_make": car_make_instances[0]
         },
         {
             "name": "A-Class",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[1],
-            "price": 35000,
+            "car_make": car_make_instances[1]
         },
         {
             "name": "C-Class",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[1],
-            "price": 40000,
+            "car_make": car_make_instances[1]
         },
         {
             "name": "E-Class",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[1],
-            "price": 80000,
+            "car_make": car_make_instances[1]
         },
         {
             "name": "A4",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[2],
-            "price": 60000,
+            "car_make": car_make_instances[2]
         },
         {
             "name": "A5",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[2],
-            "price": 80000,
+            "car_make": car_make_instances[2]
         },
         {
             "name": "A6",
             "type": "SUV",
             "year": 2023,
             "car_make": car_make_instances[2],
-            "price": 120000,
         },
         {
             "name": "Sorrento",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[3],
-            "price": 25000,
+            "car_make": car_make_instances[3]
         },
         {
             "name": "Carnival",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[3],
-            "price": 36000,
+            "car_make": car_make_instances[3]
         },
         {
             "name": "Cerato",
             "type": "Sedan",
             "year": 2023,
-            "car_make": car_make_instances[3],
-            "price": 64000,
+            "car_make": car_make_instances[3]
         },
         {
             "name": "Corolla",
             "type": "Sedan",
             "year": 2023,
-            "car_make": car_make_instances[4],
-            "price": 12000,
+            "car_make": car_make_instances[4]
         },
         {
             "name": "Camry",
             "type": "Sedan",
             "year": 2023,
-            "car_make": car_make_instances[4],
-            "price": 35000,
+            "car_make": car_make_instances[4]
         },
         {
             "name": "Kluger",
             "type": "SUV",
             "year": 2023,
-            "car_make": car_make_instances[4],
-            "price": 68000,
+            "car_make": car_make_instances[4]
         },
     ]
 
@@ -135,6 +118,5 @@ def initiate():
             name=data["name"],
             car_make=data["car_make"],
             type=data["type"],
-            year=data["year"],
-            price=data["price"],
+            year=data["year"]
         )
